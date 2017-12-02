360ff1894a6e386ef30d8086d22281b2941c76d8
mcraigie <max.craigie@gmail.com>
2017-12-02 17:19:02 +1100

    implement day 2 solutions

---
+++ problems/day_2.rb
@@ -0,0 +1,20 @@
+# Author:: Max Craigie  (@MaxCraigie)
+
+class Day2
+  def self.problem_1(input)
+    spreadsheet = input.each_line.map { |line| line.split.map(&:to_i) }
+    spreadsheet.map { |row| row.max - row.min }.sum
+  end
+
+  def self.problem_2(input)
+    spreadsheet = input.each_line.map { |line| line.split.map(&:to_i) }
+
+    spreadsheet.map do |row|
+      row.map do |divisor|
+        row.map do |dividend|
+          dividend != divisor && dividend % divisor == 0  ? dividend / divisor : 0
+        end
+      end
+    end.flatten.sum
+  end
+end
