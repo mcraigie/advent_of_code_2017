b3a06d1ccba3e6c0fc668ed68d63ed1ef3ac0148
mcraigie <max.craigie@gmail.com>
2017-12-03 19:23:35 +1100

    clean up

--- problems/day_3.rb
+++ problems/day_3.rb
@@ -7,13 +7,13 @@ class Day3
     current_number = 1
     current_offset = [0, 0]
 
-    momentum = 1 # number of times current direction be repeated
+    momentum = 1 # number of times current direction is repeated
     countdown = momentum
     allow_momentum_increase = false
 
     while current_number < target_number.to_i
       current_number += 1
-      current_offset.map!.with_index{ |e, i| e + transforms.first[i] }
+      current_offset.map!.with_index { |e, i| e + transforms.first[i] }
 
       countdown -= 1
 
@@ -23,14 +23,14 @@ class Day3
 
         # increase momentum every second time we change direction
         allow_momentum_increase = !allow_momentum_increase
-        momentum += 1 if allow_momentum_increase 
+        momentum += 1 if allow_momentum_increase
       end
     end
 
     current_offset.sum(&:abs)
   end
 
-  def self.problem_2(input)
+  # def self.problem_2(input)
 
-  end
+  # end
 end
