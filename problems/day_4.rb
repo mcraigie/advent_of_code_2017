79700326a5340d43daed379d23394ae1b53f0ad0
mcraigie <max.craigie@gmail.com>
2017-12-04 18:16:18 +1100

    refactor day 4

--- problems/day_4.rb
+++ problems/day_4.rb
@@ -3,14 +3,15 @@
 class Day4
   def self.problem_1(input)
     input.each_line.count do |passphrase|
-      passphrase.split.all? { |word| passphrase.split.count(word) == 1 }
+      split_passphrase = passphrase.split
+      split_passphrase.all? { |word| split_passphrase.count(word) == 1 }
     end
   end
 
   def self.problem_2(input)
     input.each_line.count do |passphrase|
-      sorted = passphrase.split.map { |word| word.chars.sort.join }
-      sorted.all? { |e| sorted.count(e) == 1 }
+      sorted_passphrase = passphrase.split.map { |word| word.chars.sort.join }
+      sorted_passphrase.all? { |e| sorted_passphrase.count(e) == 1 }
     end
   end
 end
