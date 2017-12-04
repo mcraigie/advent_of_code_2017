f2745cd24ea0f0d30a302726f4fb4f29db4be00d
mcraigie <max.craigie@gmail.com>
2017-12-04 17:02:52 +1100

    simplify day 4 solutions

--- problems/day_4.rb
+++ problems/day_4.rb
@@ -3,14 +3,14 @@
 class Day4
   def self.problem_1(input)
     input.each_line.count do |passphrase|
-      passphrase.split.all? { |word| passphrase.split.count(word) < 2 }
+      passphrase.split.all? { |word| passphrase.split.count(word) == 1 }
     end
   end
 
   def self.problem_2(input)
     input.each_line.count do |passphrase|
       sorted = passphrase.split.map { |word| word.chars.sort.join }
-      sorted.all? { |e| sorted.count(e) < 2 }
+      sorted.all? { |e| sorted.count(e) == 1 }
     end
   end
 end
