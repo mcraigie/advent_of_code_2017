da6c1a04878c003daf5d71d7720d718b59d47091
mcraigie <max.craigie@gmail.com>
2017-12-05 16:23:04 +1100

    clean up tests

--- spec/day_1_spec.rb
+++ spec/day_1_spec.rb
@@ -2,51 +2,42 @@
 
 describe 'Day 1' do
   describe 'Problem 1' do
-    it 'calculates the correct captcha from 1122' do
-      input = '1122'
-      expect(Day1.problem_1(input)).to eq(3)
+    it 'calculates the correct captcha' do
+      expect(Day1.problem_1('1122')).to eq(3)
     end
 
-    it 'calculates the correct captcha from 1111' do
-      input = '1111'
-      expect(Day1.problem_1(input)).to eq(4)
+    it 'calculates the correct captcha' do
+      expect(Day1.problem_1('1111')).to eq(4)
     end
 
-    it 'calculates the correct captcha from 1234' do
-      input = '1234'
-      expect(Day1.problem_1(input)).to eq(0)
+    it 'calculates the correct captcha' do
+      expect(Day1.problem_1('1234')).to eq(0)
     end
 
-    it 'calculates the correct captcha from 91212129' do
-      input = '91212129'
-      expect(Day1.problem_1(input)).to eq(9)
+    it 'calculates the correct captcha' do
+      expect(Day1.problem_1('91212129')).to eq(9)
     end
   end
 
   describe 'Problem 2' do
-    it 'calculates the correct captcha from 1212' do
-      input = '1212'
-      expect(Day1.problem_2(input)).to eq(6)
+    it 'calculates the correct captcha' do
+      expect(Day1.problem_2('1212')).to eq(6)
     end
 
-    it 'calculates the correct captcha from 1221' do
-      input = '1221'
-      expect(Day1.problem_2(input)).to eq(0)
+    it 'calculates the correct captcha' do
+      expect(Day1.problem_2('1221')).to eq(0)
     end
 
-    it 'calculates the correct captcha from 123425' do
-      input = '123425'
-      expect(Day1.problem_2(input)).to eq(4)
+    it 'calculates the correct captcha' do
+      expect(Day1.problem_2('123425')).to eq(4)
     end
 
-    it 'calculates the correct captcha from 123123' do
-      input = '123123'
-      expect(Day1.problem_2(input)).to eq(12)
+    it 'calculates the correct captcha' do
+      expect(Day1.problem_2('123123')).to eq(12)
     end
 
-    it 'calculates the correct captcha from 12131415' do
-      input = '12131415'
-      expect(Day1.problem_2(input)).to eq(4)
+    it 'calculates the correct captcha' do
+      expect(Day1.problem_2('12131415')).to eq(4)
     end
   end
 end
--- spec/day_3_spec.rb
+++ spec/day_3_spec.rb
@@ -3,40 +3,33 @@
 describe 'Day 3' do
   describe 'Problem 1' do
     it 'calculates the offset of 1' do
-      input = "1"
-      expect(Day3.problem_1(input)).to eq(0)
+      expect(Day3.problem_1('1')).to eq(0)
     end
 
     it 'calculates the offset of 12' do
-      input = "12"
-      expect(Day3.problem_1(input)).to eq(3)
+      expect(Day3.problem_1('12')).to eq(3)
     end
 
     it 'calculates the offset of 23' do
-      input = "23"
-      expect(Day3.problem_1(input)).to eq(2)
+      expect(Day3.problem_1('23')).to eq(2)
     end
 
     it 'calculates the offset of 1024' do
-      input = "1024"
-      expect(Day3.problem_1(input)).to eq(31)
+      expect(Day3.problem_1('1024')).to eq(31)
     end
   end
 
   describe 'Problem 2' do
     it 'calculates the first number greater than 20' do
-      input = "20"
-      expect(Day3.problem_2(input)).to eq(23)
+      expect(Day3.problem_2('20')).to eq(23)
     end
 
     it 'calculates the first number greater than 100' do
-      input = "100"
-      expect(Day3.problem_2(input)).to eq(122)
+      expect(Day3.problem_2('100')).to eq(122)
     end
 
     it 'calculates the first number greater than 800' do
-      input = "800"
-      expect(Day3.problem_2(input)).to eq(806)
+      expect(Day3.problem_2('800')).to eq(806)
     end
   end
 end
