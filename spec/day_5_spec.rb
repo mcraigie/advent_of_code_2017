ff4de2bfe986626703e08c87c8a09d02f6a97f00
mcraigie <max.craigie@gmail.com>
2017-12-05 16:23:48 +1100

    clean up day 5 test

--- spec/day_5_spec.rb
+++ spec/day_5_spec.rb
@@ -3,14 +3,12 @@
 describe 'Day 5' do
   describe 'Problem 1' do
     it 'exits the jump instruction list in 5 steps' do
-      input = 
       expect(Day5.problem_1("0\n3\n0\n1\n-3")).to eq(5)
     end
   end
 
   describe 'Problem 2' do
     it 'exits the jump instruction list in 10 steps' do
-      input = 
       expect(Day5.problem_2("0\n3\n0\n1\n-3")).to eq(10)
     end
   end
